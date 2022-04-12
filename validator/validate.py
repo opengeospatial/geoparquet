409e3a02002e278706b25a9935afdc39969fd5fb
Kyle Barron <kbarron@foursquare.com>
2022-04-11 18:49:41 -0600

    Validator updates

--- validator/validate.py
+++ validator/validate.py
@@ -1,11 +1,50 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
-import sys
 import json
-import pyarrow.parquet as pq
-
+import sys
 from pathlib import Path
+from pprint import pprint
+from urllib.parse import urlparse
+
+import pyarrow.parquet as pq
+from fsspec import AbstractFileSystem
+from fsspec.implementations.local import LocalFileSystem
 from jsonschema.validators import Draft7Validator
+from pyarrow.fs import FSSpecHandler, PyFileSystem
+
+
+class MetadataError(ValueError):
+    pass
+
+
+def choose_fsspec_fs(url_or_path: str) -> AbstractFileSystem:
+    """Choose fsspec filesystem by sniffing input url"""
+    parsed = urlparse(url_or_path)
+
+    if parsed.scheme.startswith("http"):
+        from fsspec.implementations.http import HTTPFileSystem
+
+        return HTTPFileSystem()
+
+    if parsed.scheme == "s3":
+        from s3fs import S3FileSystem
+
+        return S3FileSystem()
+
+    if parsed.scheme == "gs":
+        from gcsfs import GCSFileSystem
+
+        return GCSFileSystem()
+
+    # TODO: Add Azure
+    return LocalFileSystem()
+
+
+def load_parquet_schema(url_or_path: str) -> pq.ParquetSchema:
+    """Load schema from local or remote Parquet file"""
+    fsspec_fs = choose_fsspec_fs(url_or_path)
+    pyarrow_fs = PyFileSystem(FSSpecHandler(fsspec_fs))
+    return pq.read_schema(pyarrow_fs.open_input_file(url_or_path))
 
 
 def validate_geoparquet(input_file):
@@ -14,7 +53,13 @@ def validate_geoparquet(input_file):
     with open(schema_path) as f:
         schema = json.load(f)
 
-    metadata = json.loads(pq.read_schema(input_file).metadata[b"geo"])
+    parquet_schema = load_parquet_schema(input_file)
+    if b"geo" not in parquet_schema.metadata:
+        raise MetadataError("Parquet file schema does not have 'geo' key")
+
+    metadata = json.loads(parquet_schema.metadata[b"geo"])
+    print("Metadata loaded from file:")
+    pprint(metadata)
 
     errors = Draft7Validator(schema).iter_errors(metadata)
 
@@ -28,7 +73,7 @@ def validate_geoparquet(input_file):
             print(f"          INFO: {error.schema['description']}")
 
     # Extra errors
-    if (metadata["primary_column"] not in metadata["columns"]):
+    if metadata["primary_column"] not in metadata["columns"]:
         valid = False
         print("- [ERROR] $.primary_column: must be in $.columns")
 
@@ -36,7 +81,8 @@ def validate_geoparquet(input_file):
         print("This is a valid GeoParquet file.")
     else:
         print("This is an invalid GeoParquet file.")
+        exit(1)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     validate_geoparquet(sys.argv[1])
