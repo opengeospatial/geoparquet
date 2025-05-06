4070f419e7f868dfcd9a2e203cae984673678e0d
Dewey Dunnington <dewey@wherobots.com>
2025-05-05 21:04:05 -0500

    Updated Python scripts (#272)
    Updated the python scripts to run with uv
    
    ---------
    
    Co-authored-by: Tom Augspurger <toaugspurger@nvidia.com>

--- scripts/generate_example.py
+++ scripts/generate_example.py
@@ -8,18 +8,22 @@ You can print the metadata with:
    >>> import json, pprint, pyarrow.parquet as pq
    >>> pprint.pprint(json.loads(pq.read_schema("example.parquet").metadata[b"geo"]))
 """
+
 from collections import OrderedDict
 import json
 import pathlib
 
+import pandas as pd
 import geopandas
 import pyarrow as pa
 import pyarrow.parquet as pq
 
 HERE = pathlib.Path(__file__).parent
 
-df = geopandas.read_file(geopandas.datasets.get_path("naturalearth_lowres"))
-df = df.to_crs("ogc:84")
+df = pd.read_csv(HERE.parent / "examples" / "example.csv")
+df = geopandas.GeoDataFrame(
+    df, geometry=geopandas.GeoSeries.from_wkt(df.geometry, crs="OGC:CRS84")
+)
 
 geometry_bbox = df.bounds.rename(
     OrderedDict(
